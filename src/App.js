a953bd697ce36996c5f236a40ddfeaeb867f3fde
dhrishika <37090857+dhrishika@users.noreply.github.com>
2023-12-13 02:36:13 +0300

    Added files

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+import Navbar from "./components/Navbar.js";
+import "./components/Navbar.css";
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar />
+      Hello World
+    </div>
+  );
+}
+
+export default App;
